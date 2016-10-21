89a82bd3a538fbb2b7d542ad150468505fc44c94
A.Y. Siu <aysiu@users.noreply.github.com>
2016-10-21 12:50:54 -0700

    Update enroll.php

--- munki-enroll/enroll.php
+++ munki-enroll/enroll.php
@@ -1,52 +1,62 @@
 <?php
-namespace CFPropertyList;
 
-require_once( 'cfpropertylist-2.0.1/CFPropertyList.php' );
+if((isset($_GET["serial"])) AND (isset($_GET["displayname"])) AND (isset($_GET["template"]))){
 
-// Get the varibles passed by the enroll script
-$identifier = $_GET["identifier"];
-$hostname   = $_GET["hostname"];
+    // Get the variables passed by the enroll script
+    $serial = $_GET["serial"];
+    $displayname = $_GET["displayname"];
+    $template = $_GET["template"];
 
-// Split the manifest path up to determine directory structure
-$directories		= explode( "/", $identifier, -1 ); 
-$total				= count( $directories );
-$n					= 0;
-$identifier_path	= "";
-while ( $n < $total )
-    {
-        $identifier_path .= $directories[$n] . '/';
-        $n++;
-    }
+    // Specify the template source to be copied as an individual template
+    if($template == "desktop"){
+    
+            $source="../manifests/desktop_template";
 
-// Check if manifest already exists for this machine
-if ( file_exists( '../manifests/' . $identifier_path . '/clients/' . $hostname ) )
-    {
-        echo "Computer manifest already exists.";
-    }
-else
-    {
-        echo "Computer manifest does not exist. Will create.";
+        } elseif($template == "laptop") {
+            
+            $source="../manifests/laptop_template";
         
-        if ( !is_dir( '../manifests/' . $identifier_path . 'clients/' ) )
-            {
-                mkdir( '../manifests/' . $identifier_path . 'clients/', 0755, true );
+        } else {
+        
+            $source="";
+        
+        }
+
+    // Double-check there is a source to copy from
+    if(($source!="") AND (file_exists($source))){
+
+        // Create destination
+        $destination="../manifests/" . $serial;
+
+        // Check if manifest already exists for this machine
+        if ( file_exists($destination) ){
+            
+                // The manifest already exists. Nothing to do.
+                echo "Computer manifest " . $serial . " already exists.";
+        
+            } else {
+            
+                // Create the manifest
+                echo "Computer manifest does not exist. Will create " . $destination;
+                copy($source, $destination);
+
+                // Add in the proper display name
+                // Comment out this command if you aren't using macOS, which you likely aren't.
+                // For Linux, you can put in a placeholder in the template to copy and then use the sed command to find/replace text
+                // Will probably go this route for future versions, so it's not tied to macOS
+                shell_exec('/usr/libexec/PlistBuddy -c "Add :display_name string ' . "'" . $displayname . "'" . '" ' . $destination);
             }
+
+        // End checking for source manifest existence
+        } else {
         
-        // Create the new manifest plist
-        $plist = new CFPropertyList();
-        $plist->add( $dict = new CFDictionary() );
+            echo "Cannot create manifest. The source template to copy from does not exist.";
         
-        // Add manifest to production catalog by default
-        $dict->add( 'catalogs', $array = new CFArray() );
-        $array->add( new CFString( 'production' ) );
-        
-        // Add parent manifest to included_manifests to achieve waterfall effect
-        $dict->add( 'included_manifests', $array = new CFArray() );
-        $array->add( new CFString( $identifier ) );
-        
-        // Save the newly created plist
-        $plist->saveXML( '../manifests/' . $identifier_path . 'clients/' . $hostname );
-        
+        }
+
+    // End checking for GET variables
+    } else {
+        echo "Cannot create manifest. Missing one or more variables. Need serial number, display name, and template";
     }
 
 ?>
